82a108aa85919deb76a64090c0ae742453c5a629
Aditya Gupta <gupadi04@gmail.com>
2025-04-14 20:59:07 +0530

    Poolformer fixes

--- tokenmixers.py
+++ tokenmixers.py
@@ -2,6 +2,22 @@ import torch
 from torch import nn
 import math
 
+# NOT Implemented in the blocks
+class GroupNorm(nn.GroupNorm):
+    """
+    Group Normalization with 1 group.
+    Input: tensor in shape [B, N, C] (code was for B,C,H,W in poolformer)
+    """
+    def __init__(self, num_channels, **kwargs):
+        super().__init__(1, num_channels, **kwargs)
+
+    def forward(self, x):
+
+        x = x.permute(0, 2, 1)  
+        x = super().forward(x)
+
+        return x.permute(0, 2, 1)
+
 class PoolingFBlock(nn.Module):
     """
     This creates the function F such that the entire block can be
@@ -28,7 +44,7 @@ class PoolingFBlock(nn.Module):
 
             x = self.norm(x)    
             x = x.transpose(1,2).reshape(batch_size, dim, self.patches_shape[0], self.patches_shape[1])
-            x = self.pool(x)
+            x = self.pool(x) - x
             x = x.reshape(batch_size, sequence_length, dim)
 
             return x
